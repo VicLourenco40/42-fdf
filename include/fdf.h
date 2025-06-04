c6d8b85b180e4c7851de91aa191865d395680e32
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2025-06-04 16:01:10 +0100

    Draw and display image

--- include/fdf.h
+++ include/fdf.h
@@ -6,13 +6,23 @@
 /*   By: vde-albu <vde-albu@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 14:28:33 by vde-albu          #+#    #+#             */
-/*   Updated: 2025/06/04 12:28:25 by vde-albu         ###   ########.fr       */
+/*   Updated: 2025/06/04 15:33:56 by vde-albu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
-# define ESCAPE 65307
+
+# define WINDOW_WIDTH 640
+# define WINDOW_HEIGHT 480
+
+# define KEY_ESCAPE 65307
+
+typedef struct s_vec2
+{
+	int	x;
+	int	y;
+}	t_vec2;
 
 typedef struct s_mlx
 {
@@ -20,10 +30,13 @@ typedef struct s_mlx
 	void	*win_ptr;
 }	t_mlx;
 
-typedef struct s_vec2
+typedef struct s_image
 {
-	int	x;
-	int	y;
-}	t_vec2;
+	void	*ptr;
+	char	*data;
+	int		line_size;
+	int		color_depth;
+	int		endian;
+}	t_image;
 
 #endif
--- src/fdf.c
+++ src/fdf.c
@@ -6,7 +6,7 @@
 /*   By: vde-albu <vde-albu@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 14:24:01 by vde-albu          #+#    #+#             */
-/*   Updated: 2025/06/04 13:12:43 by vde-albu         ###   ########.fr       */
+/*   Updated: 2025/06/04 15:57:38 by vde-albu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int	free_exit(t_mlx *mlx)
 
 int	key_handler(int keycode, void *param)
 {
-	if (keycode == ESCAPE)
+	if (keycode == KEY_ESCAPE)
 		free_exit(param);
 	return (0);
 }
@@ -36,7 +36,15 @@ int	loop(void)
 	return (0);
 }
 
-void	draw_line(const t_vec2 p1, const t_vec2 p2, t_mlx *mlx)
+void	put_pixel(t_image *const image, const t_vec2 pos)
+{
+	if (pos.x < 0 || pos.y < 0 || \
+		pos.x >= WINDOW_WIDTH || pos.y >= WINDOW_HEIGHT)
+		return ;
+	((int *)(image->data + pos.y * image->line_size))[pos.x] = 0xFFFFFFFF;
+}
+
+void	put_line(t_image *const image, const t_vec2 p1, const t_vec2 p2)
 {
 	const t_vec2	delta = {p2.x - p1.x, p2.y - p1.y};
 	const int		step = ft_max(ft_abs(delta.x), ft_abs(delta.y));
@@ -44,32 +52,30 @@ void	draw_line(const t_vec2 p1, const t_vec2 p2, t_mlx *mlx)
 	float			step_y;
 	int				i;
 
-	mlx_pixel_put(mlx->ptr, mlx->win_ptr, p1.x, p1.y, 0xFFFFFFFF);
+	put_pixel(image, p1);
 	if (step == 0)
 		return ;
 	step_x = (float)delta.x / step;
 	step_y = (float)delta.y / step;
 	i = 0;
 	while (++i <= step)
-		mlx_pixel_put(mlx->ptr, mlx->win_ptr, roundf(p1.x + step_x * i), \
-			roundf(p1.y + step_y * i), 0xFFFFFFFF);
+		put_pixel(image, \
+			(t_vec2){roundf(p1.x + step_x * i), roundf(p1.y + step_y * i)});
 }
 
 int	main(int argc, char **argv)
 {
 	t_mlx	mlx;
+	t_image	image;
 
 	mlx.ptr = mlx_init();
-	if (!mlx.ptr)
-		return (1);
-	mlx.win_ptr = mlx_new_window(mlx.ptr, 640, 480, "fdf");
-	if (!mlx.win_ptr)
-		return (1);
-	draw_line((t_vec2){4, 8}, (t_vec2){3, 3}, &mlx);
-	draw_line((t_vec2){-10, -10}, (t_vec2){1, 1}, &mlx);
-	draw_line((t_vec2){10, 10}, (t_vec2){10, 10}, &mlx);
-	draw_line((t_vec2){20, 20}, (t_vec2){20, 30}, &mlx);
-	draw_line((t_vec2){20, 20}, (t_vec2){30, 20}, &mlx);
+	mlx.win_ptr = mlx_new_window(mlx.ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "fdf");
+	image.ptr = mlx_new_image(mlx.ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
+	image.data = mlx_get_data_addr(image.ptr, &image.color_depth, \
+		&image.line_size, &image.endian);
+	put_line(&image, (t_vec2){-1000, -1000}, (t_vec2){10000, 10000});
+	mlx_put_image_to_window(mlx.ptr, mlx.win_ptr, image.ptr, 0, 0);
+	mlx_destroy_image(mlx.ptr, image.ptr);
 	mlx_key_hook(mlx.win_ptr, &key_handler, &mlx);
 	mlx_loop_hook(mlx.ptr, &loop, NULL);
 	mlx_loop(mlx.ptr);
